0020edbd1d40f97b257880f0184cdfb6bcb20604
adam wong <adamwong246@gmail.com>
2015-09-02 15:12:56 -0700

    tweak name

--- package.js
+++ package.js
@@ -18,7 +18,7 @@ Package.onUse(function(api) {
     'less',
     'aldeed:autoform@5.3.2',
     'fortawesome:fontawesome@4.3.0',
-    'mdg:camera',
+    "chroma:camera",
     'awatson1978:browser-detection'
   ]);
 
